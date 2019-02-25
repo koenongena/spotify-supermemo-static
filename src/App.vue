d3e899ffdee5db2163f2a7729d503a7ed021dae3
Koen Ongena <koen.ongena@axians.com>
2019-02-25 21:42:21 +0100

    Ttypefoutje

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
             <router-link to="/home">Home</router-link>
             <router-link to="/schedule">Schedule</router-link>
             <router-link to="/import">Import</router-link>
-            <router-link to="/spotify-playlists">Spotify playlilsts</router-link>
+            <router-link to="/spotify-playlists">Spotify playlists</router-link>
             <router-link to="/buffer">Buffer</router-link>
         </aside>
         <main>
